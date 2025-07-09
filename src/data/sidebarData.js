51f153c95ed69d33492299492d9f7589a552e0c0
Ernest <zyperis14@gmail.com>
2025-07-09 16:20:26 +0300

    categories table

--- src/data/sidebarData.js
+++ src/data/sidebarData.js
@@ -2,18 +2,18 @@ export const sidebarMenuData = [
     [
         { href: '/admin', text: 'Dashboard' },
     ],
-    'Categories',
-    [
-        { href: '/admin/categories/new', text: 'New categories' },
-        { href: '/admin/categories', text: 'Categories' },
-        { href: '/admin/categories/draft', text: 'Draft categories' },
-        { href: '/admin/categories/published', text: 'Published categories' },
-    ],
     'Movies',
     [
-        { href: '/admin/movies/new', text: 'New movies' },
-        { href: '/admin/movies', text: 'Movies' },
-        { href: '/admin/movies/draft', text: 'Draft movies' },
+        { href: '/admin/movies/new', text: 'New movie' },
+        { href: '/admin/movies', text: 'All movies' },
         { href: '/admin/movies/published', text: 'Published movies' },
-    ]
+        { href: '/admin/movies/draft', text: 'Draft movies' },
+    ],
+    'Categories',
+    [
+        { href: '/admin/categories/new', text: 'New category' },
+        { href: '/admin/categories', text: 'All categories' },
+        { href: '/admin/categories/published', text: 'Published categories' },
+        { href: '/admin/categories/draft', text: 'Draft categories' },
+    ],
 ];
